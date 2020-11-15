be9160863400e3fa845788ce869bebf8954a1140
Vidalee <thomas.sirvent@free.fr>
2020-11-15 21:31:11 +0100

    make navbar working

--- client/static/navbar.js
+++ client/static/navbar.js
@@ -4,16 +4,17 @@ document.addEventListener('DOMContentLoaded', () => {
     document.querySelectorAll('.navbar-burger'),
     0
   )
-
+    console.log("euh");
   // Check if there are any navbar burgers
   if ($navbarBurgers.length > 0) {
     // Add a click event on each of them
+    console.log("et d'un");
     $navbarBurgers.forEach(el => {
       el.addEventListener('click', () => {
         // Get the target from the "data-target" attribute
         const target = el.dataset.target
         const $target = document.getElementById(target)
-
+        console.log("toggle", target);
         // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
         el.classList.toggle('is-active')
         $target.classList.toggle('is-active')
